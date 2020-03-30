419704f69281b389a718c1dd1d32b4c1203dd3d2
Cora Sherratt <cora.sherratt@gmail.com>
2020-03-30 01:28:53 -0400

    added sharding

--- benches/suits/legion_parallel.rs
+++ benches/suits/legion_parallel.rs
@@ -12,7 +12,21 @@ pub struct XY(u32, u32);
 pub struct Rounds(u32);
 
 #[derive(Copy, Clone, Debug, Default, PartialEq, Eq)]
-pub struct Shard(u32);
+pub struct ShardA;
+#[derive(Copy, Clone, Debug, Default, PartialEq, Eq)]
+pub struct ShardB;
+#[derive(Copy, Clone, Debug, Default, PartialEq, Eq)]
+pub struct ShardC;
+#[derive(Copy, Clone, Debug, Default, PartialEq, Eq)]
+pub struct ShardD;
+#[derive(Copy, Clone, Debug, Default, PartialEq, Eq)]
+pub struct ShardE;
+#[derive(Copy, Clone, Debug, Default, PartialEq, Eq)]
+pub struct ShardF;
+#[derive(Copy, Clone, Debug, Default, PartialEq, Eq)]
+pub struct ShardG;
+#[derive(Copy, Clone, Debug, Default, PartialEq, Eq)]
+pub struct ShardH;
 
 pub fn legion_mandelbrot(group: &mut BenchmarkGroup<WallTime>, width: u32, height: u32) {
     fn run_mandelbrot(bencher: &mut Bencher<WallTime>, mut world: World) {
@@ -47,28 +61,36 @@ pub fn legion_mandelbrot(group: &mut BenchmarkGroup<WallTime>, width: u32, heigh
                     (XY(x, y), Coordinate(xf, yf), Rounds(0))
                 })
         );
-
         run_mandelbrot(bencher, world);
     });
 
-    group.bench_function("legion-sharded", |bencher| {
+
+    group.bench_function("legions-sharded", |bencher| {
         let mut world = World::new();
-        let shard_size = height / 64;
-        for (x, shard) in (0..64).map(|x| (x * shard_size, Shard(x))) {
-            world.insert(
-                (shard,),
-                (x..(x+ shard_size))
-                    .flat_map(|x| (0..height).map(move |y| (x, y)))
-                    .map(|(x, y)| {
-                        let (xf, yf) = (
-                            x as f32 / width as f32 * 3.5 - 2.5,
-                            y as f32 / height as f32 * 2. - 1.
-                        );
-                        (XY(x, y), Coordinate(xf, yf), Rounds(0))
-                    })
-            );
+        let entities: Vec<_> = world.insert(
+            (),
+            (0..width)
+                .flat_map(|x| (0..height).map(move |y| (x, y)))
+                .map(|(x, y)| {
+                    let (xf, yf) = (
+                        x as f32 / width as f32 * 3.5 - 2.5,
+                        y as f32 / height as f32 * 2. - 1.
+                    );
+                    (XY(x, y), Coordinate(xf, yf), Rounds(0))
+                })
+        ).into();
+        for (i, entity) in entities.into_iter().enumerate() {
+            let n = i % 256;
+            if n & 1 != 0 { world.add_component(entity, ShardA); }
+            if n & 2 != 0 { world.add_component(entity, ShardB); }
+            if n & 4 != 0 { world.add_component(entity, ShardC); }
+            if n & 8 != 0 { world.add_component(entity, ShardD); }
+            if n & 16 != 0 { world.add_component(entity, ShardE); }
+            if n & 32 != 0 { world.add_component(entity, ShardF); }
+            if n & 64 != 0 { world.add_component(entity, ShardG); }
+            if n & 128 != 0 { world.add_component(entity, ShardH); }
         }
-
         run_mandelbrot(bencher, world);
     });
+
 }
