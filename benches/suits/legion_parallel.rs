691217d00ea8024c9120dc7d7d0b5ec6c0a35f12
Cora Sherratt <cora.sherratt@gmail.com>
2020-03-29 21:44:34 -0400

    Added legion sharded

--- benches/suits/legion_parallel.rs
+++ benches/suits/legion_parallel.rs
@@ -1,4 +1,4 @@
-use criterion::BenchmarkGroup;
+use criterion::{BenchmarkGroup, Bencher};
 use criterion::measurement::WallTime;
 use legion::prelude::*;
 
@@ -11,22 +11,11 @@ pub struct XY(u32, u32);
 #[derive(Copy, Clone, Debug, Default)]
 pub struct Rounds(u32);
 
-pub fn legion_mandelbrot(group: &mut BenchmarkGroup<WallTime>, width: u32, height: u32) {
-    group.bench_function("legion", |bencher| {
-        let mut world = World::new();
-        world.insert(
-            (),
-            (0..width)
-                .flat_map(|x| (0..height).map(move |y| (x, y)))
-                .map(|(x, y)| {
-                    let (xf, yf) = (
-                        x as f32 / width as f32 * 3.5 - 2.5,
-                        y as f32 / height as f32 * 2. - 1.
-                    );
-                    (XY(x, y), Coordinate(xf, yf), Rounds(0))
-                })
-        );
+#[derive(Copy, Clone, Debug, Default, PartialEq, Eq)]
+pub struct Shard(u32);
 
+pub fn legion_mandelbrot(group: &mut BenchmarkGroup<WallTime>, width: u32, height: u32) {
+    fn run_mandelbrot(bencher: &mut Bencher<WallTime>, mut world: World) {
         let query = <(Read<Coordinate>, Write<Rounds>)>::query();
         bencher.iter(|| {
             query.par_for_each(&mut world, |(coord, mut rounds)| {
@@ -42,5 +31,44 @@ pub fn legion_mandelbrot(group: &mut BenchmarkGroup<WallTime>, width: u32, heigh
                 *rounds = Rounds(i);
             });
         });
+    }
+
+    group.bench_function("legion", |bencher| {
+        let mut world = World::new();
+        world.insert(
+            (),
+            (0..width)
+                .flat_map(|x| (0..height).map(move |y| (x, y)))
+                .map(|(x, y)| {
+                    let (xf, yf) = (
+                        x as f32 / width as f32 * 3.5 - 2.5,
+                        y as f32 / height as f32 * 2. - 1.
+                    );
+                    (XY(x, y), Coordinate(xf, yf), Rounds(0))
+                })
+        );
+
+        run_mandelbrot(bencher, world);
+    });
+
+    group.bench_function("legion-sharded", |bencher| {
+        let mut world = World::new();
+        let shard_size = height / 64;
+        for (x, shard) in (0..64).map(|x| (x * shard_size, Shard(x))) {
+            world.insert(
+                (shard,),
+                (x..(x+ shard_size))
+                    .flat_map(|x| (0..height).map(move |y| (x, y)))
+                    .map(|(x, y)| {
+                        let (xf, yf) = (
+                            x as f32 / width as f32 * 3.5 - 2.5,
+                            y as f32 / height as f32 * 2. - 1.
+                        );
+                        (XY(x, y), Coordinate(xf, yf), Rounds(0))
+                    })
+            );
+        }
+
+        run_mandelbrot(bencher, world);
     });
 }
